e88fcefda0207efbcddb4526e27813d2494e55c4
stoopfrench <32528005+stoopfrench@users.noreply.github.com>
2017-12-25 14:43:41 -0700

    deploy 0.0.5

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const HTTP = require('http')
+const HTTPS = require('https')
 
 const app = express()
 
@@ -13,19 +14,42 @@ app.get('/', function(req, res){
 
 //==================================================
 
-var port = 80
+try {
+    var httpsConfig = {
+        key: fs.readFileSync('/etc/letsencrypt/live/iamaaronallen.com/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/iamaaronallen.com/fullchain.pem'),
+    }
 
-var counter
+    var httpsServer = HTTPS.createServer(httpsConfig, app)
+    httpsServer.listen(443)
+    var httpApp = express()
+    httpApp.use(function(req, res, next){
+        res.redirect('https://iamaaronallen.com' + req.url)
+    })
+    httpApp.listen(80)
+}
+catch(e){
+    console.log(e)
+    console.log('could not start HTTPS server')
+    var httpServer = HTTP.createServer(app)
+    httpServer.listen(80)
+}
+
+// var port = 80
+
+// var counter
 
 // app.listen(port, function(){
 
 // 	console.log('portfolio running on ', port)
 // })
 
-var httpServer = HTTP.createServer(app, function(req, res){
 
-	counter++
 
-	console.log('counter ', counter)
+// var httpServer = HTTP.createServer(app, function(req, res){
 
-}).listen(port)
+// 	counter++
+
+// 	console.log('counter ', counter)
+
+// }).listen(port)
